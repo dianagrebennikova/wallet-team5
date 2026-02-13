3d3b70c342e211ab8a8a7d2c199458dd54b3d17d
Rio-Meow <rrrio.meow@gmail.com>
2026-02-13 17:14:46 +0700

    Исправлены файлы Analytics.jsx и Analytics.styled.js.

--- src/components/Analytics/Analytics.jsx
+++ src/components/Analytics/Analytics.jsx
@@ -1,6 +1,6 @@
 import * as S from './Analytics.styled'
-Верстка-календаря-на-странице-Анализов-без-логики
 import Calendar from '../Calendar/Calendar'
+import { ExpensesChart } from '../ExpensesChart/ExpensesChart'
 
 const Analytics = () => {
     return (
@@ -11,27 +11,19 @@ const Analytics = () => {
                 <S.CalendarColumn>
                     <Calendar selectedDate="16.01.2026" onDateSelect={(date) => console.log(date)} />                    
                 </S.CalendarColumn>
+
+                <S.ContentContainer>
+                    <ExpensesChart />
+                </S.ContentContainer>                
             </S.ColumnsLayout>
         </S.AnalyticsContainer>
 
-import { ExpensesChart } from '../ExpensesChart/ExpensesChart'
-// const [period, setPeriod] = useState({ from: '2025-04-01', to: '2025-04-07' })
-//убрать коммиты после создания и импорта календаря
-
-const Analytics = () => {
-    return (
-        <>
-            <S.BodyContainer>
-                <S.AnalyticsTitle>Анализ расходов</S.AnalyticsTitle>
-                <S.ContentContainer>
-                    {/* <Calendar onPeriodChange={setPeriod} />
-                <ExpensesChart period={period} /> */}
-                    <ExpensesChart />
-                </S.ContentContainer>
-            </S.BodyContainer>
-        </>
-main
-    )
-}
+)}
 
 export default Analytics
+
+// const [period, setPeriod] = useState({ from: '2025-04-01', to: '2025-04-07' 
+//убрать коммиты после создания и импорта календаря
+
+
+
--- src/components/Analytics/Analytics.styled.js
+++ src/components/Analytics/Analytics.styled.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
 
-Верстка-календаря-на-странице-Анализов-без-логики
 export const AnalyticsContainer = styled.div`
     padding-right: calc(50% - 600px);
     padding-left: calc(50% - 600px);
-    padding-top: 36px;
+    padding-top: 100px; // 64px(высота header) + 36px(отступ заголовка сверху по макету)
     padding-bottom: 80px;
     background-color: rgba(244, 245, 246, 1);
+    box-sizing: border-box;
+
+    @media (max-width: 768px) {
+        padding-left: 16px;
+        padding-right: 16px;
+    }
 `
 
 export const AnalyticsTitle = styled.h1`
@@ -20,6 +25,18 @@ export const AnalyticsTitle = styled.h1`
     text-align: left;
 
     @media screen and (max-width: 1024px) {
+        margin-bottom: 32px;
+        border-radius: 0;
+        height: 646px;
+        text-align: center; //???
+    }
+
+    @media (max-width: 768px) {
+        width: 226px;
+        height: 29px;
+        padding-top: 24px;
+        font-size: 24px;
+        line-height: 29px;
         text-align: center;
     }
 `
@@ -43,47 +60,9 @@ export const CalendarColumn = styled.div`
     overflow: hidden;
     border-radius: 30px;
     box-shadow: 0 4px 20px rgba(0, 0, 0, 0.08);
-
-export const BodyContainer = styled.div`
-    padding-left: 120px;
-    padding-right: 120px;
-    box-sizing: border-box;
-    @media (max-width: 768px) {
-        padding-left: 16px;
-        padding-right: 16px;
-    }
 `
-export const AnalyticsTitle = styled.h1`
-    color: rgba(0, 0, 0, 1);
-    font-family: Montserrat;
-    font-style: Bold;
-    font-size: 32px;
-    font-weight: 700;
-    line-height: 150%;
-    letter-spacing: 0px;
-    text-align: left;
-    padding-top: 36px;
-main
 
-    @media screen and (max-width: 1024px) {
-        margin-bottom: 32px;
-        border-radius: 0;
-        height: 646px;
-    }
-    @media (max-width: 768px) {
-        width: 226px;
-        height: 29px;
-        padding-top: 24px;
-        color: rgba(0, 0, 0, 1);
-        font-family: Montserrat;
-        font-style: Bold;
-        font-size: 24px;
-        font-weight: 700;
-        line-height: 29px;
-        letter-spacing: 0px;
-        text-align: center;
-    }
-`
 export const ContentContainer = styled.div`
-    padding-top: 32px;
+    height: 540px;
+    flex-grow: 1;
 `
